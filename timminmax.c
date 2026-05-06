b701d9aab5918fe1e1ae72d74dded86cdef16e00
tranquan1102007-ops <tranquan1102007@gmail.com>
2026-05-06 07:01:06 +0700

    kk

--- timminmax.c
+++ timminmax.c
@@ -1,33 +1,73 @@
 #include <stdio.h>
 
-int main(){
-    int n;
-    int a[100];
-    int max, min, posMax, posMin;
-    printf("hay nhap n: ");
+#define MAX 100
+
+// Hàm nhập mảng
+int nhapMang(int a[]) {
+    int i, n;
+    printf("Nhap so luong phan tu: ");
     scanf("%d", &n);
-    for (int i=0;i<n;i++){
-            printf("a[%d] = ", i);
+    for (i = 0; i < n; i++) {
+        printf("Nhap phan tu a[%d]: ", i);
         scanf("%d", &a[i]);
     }
+    return n;
+}
+
+// Hàm in mảng
+void inMang(int a[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int a[MAX], n, i;
+    int max, min;
+
+    n = nhapMang(a);
+
+    // Kiểm tra nếu mảng rỗng thì thoát
+    if (n <= 0) {
+        return 0;
+    }
+
+    // Khởi tạo max, min là phần tử đầu tiên
     max = a[0];
     min = a[0];
 
-    for (int i = 1; i < n; i++) {
-    if (a[i] > max) { // Nếu tìm thấy số lớn hơn max hiện tại
-        max = a[i];   // Cập nhật giá trị max mới
+    // Thuật toán tìm max, min dùng if...else rõ ràng
+    for (i = 1; i < n; i++) {
+        // Tìm Max
+        if (a[i] > max) {
+            max = a[i];
+        }
 
+        // Tìm Min
+        if (a[i] < min) {
+            min = a[i];
+        }
     }
 
-}
-for (int i = 1; i < n; i++) {
-    if (a[i] < min) {
-        min = a[i];
-
+    // In kết quả Max
+    printf("\nGia tri lon nhat cua mang la %d tai vi tri: ", max);
+    for (i = 0; i < n; i++) {
+        if (a[i] == max) {
+            printf("%d ", i + 1);
+        }
     }
+    printf("\n");
 
-}
-printf("max la: %d \n", max);
-printf("min la: %d", min);
-return 0;
+    // In kết quả Min
+    printf("Gia tri nho nhat cua mang la %d tai vi tri: ", min);
+    for (i = 0; i < n; i++) {
+        if (a[i] == min) {
+            printf("%d ", i + 1);
+        }
+    }
+    printf("\n");
+
+    return 0;
 }
