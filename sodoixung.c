a239fa7a5c49edc4d0f1e0088903c7d40a48c34e
tranquan1102007-ops <tranquan1102007@gmail.com>
2026-05-10 10:49:50 +0700

    kk

--- sodoixung.c
+++ sodoixung.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
-#include <math.h>
-int sodoixung(int n){
-    int somoi;
-    int temp=n;
-    while(temp>0){
-        int dem=1;
-        while(temp>0){
-            temp=temp/10;
-            dem++;
-        }
-        somoi=somoi+(temp%10)*pow(10, dem-1);
+
+int sodoixung(int n) {
+    int somoi = 0;
+    int temp = n;
+
+    while (temp > 0) {
+        int chu_so = temp % 10;           // Lấy chữ số cuối
+        somoi = somoi * 10 + chu_so;      // Đẩy vào số mới
+        temp = temp / 10;                 // Bỏ chữ số cuối của temp
     }
+
     return somoi;
 }
-int main(){
+
+int main() {
     int n;
-    printf("nhap n: ");
-    scanf("%d" ,&n);
-    if(sodoixung(n)==n){
-        printf("dung");
+    printf("Nhap n: ");
+    if (scanf("%d", &n) != 1) return 1; // Kiểm tra đầu vào hợp lệ
+
+    // Nếu n âm, thường không xét là số đối xứng,
+    // nhưng nếu bạn muốn xét, hãy lấy giá trị tuyệt đối trước.
+    if (n >= 0 && sodoixung(n) == n) {
+        printf("dung\n");
+    } else {
+        printf("sai\n");
     }
-    else {
-        printf("sai");
-    }
+
     return 0;
 }
